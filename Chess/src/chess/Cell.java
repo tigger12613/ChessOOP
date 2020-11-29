6403b1b9d6298cca8e6a1a80ed648ce9c0a91e40
tigger12613 <c14066060@gs.ncku.edu.tw>
2020-11-29 14:05:37 +0800

    Remove piece in Cell
    Cell should only be a view. So it should not have piece to
    indicate the chess on the Cell. What that the Chess cell should
    displayed should be control by the Controller.

--- Chess/src/chess/Cell.java
+++ Chess/src/chess/Cell.java
@@ -16,7 +16,6 @@ public class Cell extends JPanel implements Cloneable {
 	private static final long serialVersionUID = 1L;
 	private boolean ispossibledestination;
 	private JLabel content;
-	private Piece piece;
 	int x, y; // is public because this is to be accessed by all the other class
 	private boolean isSelected = false;
 	private boolean ischeck = false;
@@ -34,49 +33,23 @@ public class Cell extends JPanel implements Cloneable {
 		else
 			setBackground(Color.white);
 
-		if (p != null)
-			setPiece(p);
 	}
 
 	// A constructor that takes a cell as argument and returns a new cell will the
 	// same data but different reference
-	public Cell(Cell cell) throws CloneNotSupportedException {
-		this.x = cell.x;
-		this.y = cell.y;
-		setLayout(new BorderLayout());
-		if ((x + y) % 2 == 0)
-			setBackground(new Color(113, 198, 113));
-		else
-			setBackground(Color.white);
-		if (cell.getpiece() != null) {
-			setPiece(cell.getpiece().getcopy());
-		} else
-			piece = null;
-	}
-
-	public void setPiece(Piece p) // Function to inflate a cell with a piece
-	{
-		piece = p;
-		ImageIcon img = new javax.swing.ImageIcon(this.getClass().getResource(p.getPath()));
-		content = new JLabel(img);
-		this.add(content);
-	}
-
-	public void removePiece() // Function to remove a piece from the cell
-	{
-		if (piece instanceof King) {
-			piece = null;
-			this.remove(content);
-		} else {
-			piece = null;
-			this.remove(content);
-		}
-	}
-
-	public Piece getpiece() // Function to access piece of a particular cell
-	{
-		return this.piece;
-	}
+	// public Cell(Cell cell) throws CloneNotSupportedException {
+	// 	this.x = cell.x;
+	// 	this.y = cell.y;
+	// 	setLayout(new BorderLayout());
+	// 	if ((x + y) % 2 == 0)
+	// 		setBackground(new Color(113, 198, 113));
+	// 	else
+	// 		setBackground(Color.white);
+	// 	if (cell.getpiece() != null) {
+	// 		setPiece(cell.getpiece().getcopy());
+	// 	} else
+	// 		piece = null;
+	// }
 
 	public void select() // Function to mark a cell indicating it's selection
 	{
